c1ec7718d5657b8a787b4365b6b3becd9b4a4ab5
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-10-06 01:54:35 -0300

    filmeContexto adicionado no startupcs services AddDb

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using aspnetcoreapp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace aspnetcoreapp
 {
@@ -24,6 +26,12 @@ namespace aspnetcoreapp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContext<FilmeContexto>(
+                opcoes=> opcoes.UseSqlite(
+                    Configuration.GetConnectionString("FilmeContexto")
+                    )
+                    );
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
